26a3fde4b8aabcf381c45525b1570533f44b29bd
ALUMNO <ALUMNO@DESKTOP-V0F7RQ5>
2026-03-19 19:29:19 -0600

    Ingresos de datos

--- Actividad 15/Program.cs
+++ Actividad 15/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Dictionary<int, Producto> producto = new Dictionary<int, Producto>();
+Dictionary<int, Producto> inventario = new Dictionary<int, Producto>();
 int opcion;
 
 do
@@ -21,8 +21,42 @@ do
     opcion=int.Parse(Console.ReadLine());
 
     switch (opcion)
-        {
+    {
         case 1:
+            Console.WriteLine("Agregar producto");
+            Console.Write("Cuantos Productos desea ingresar");
+            int n=int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Producto e = new Producto();
+                Console.WriteLine($"\n Producto {i + 1} ");
+                Console.Write("Codigo del Producto:_ ");
+                e.codigo=int.Parse(Console.ReadLine());
+                if (inventario.ContainsKey(e.codigo))
+                {
+                    Console.WriteLine("Codigo ya existente");
+                    break;
+                }
+                else
+                {
+                    Console.Write("Nombre del producto:_ ");
+                    e.nombre = Console.ReadLine();
+                    Console.Write("Precio:_");
+                    e.precio=double.Parse(Console.ReadLine());
+                    Console.Write("Cantidad:_ ");
+                    e.cantidadExistencia=int.Parse(Console.ReadLine());
+                    
+                }
+                if (e.precio<=0|| e.cantidadExistencia<=0)
+                {
+                        Console.WriteLine(" No puede ser menor a cero");
+                    break;
+               
+                }
+                inventario.Add(e.codigo,e);
+               
+            }
+
             break;
             case 2:
             break;
@@ -35,6 +69,21 @@ do
             case 6:
             break;
         case 7:
+            break;
+            case 8:
+            break;
+            case 9:
+            break;
+            case 10:
+            break;
+            case 11:
+            Console.WriteLine("¡Hasta Pronto!.");
+            Console.WriteLine("Preciona cualquir letra para salir");
+            Console.ReadKey();
+            break;
+            default:
+            Console.WriteLine("Opcion no valida, Precione cualquier letra");
+            Console.ReadKey();
             break;
 
     }
@@ -54,6 +103,14 @@ public class Producto
     public double precio;
     public double cantidadExistencia;
 
+    public void MostrarDatos()
+    {
+        Console.WriteLine($"Codigo:_ {codigo}");
+        Console.WriteLine($" Nombre:_ {nombre}");
+        Console.WriteLine($"Precio:_ {precio:F2}");
+        Console.WriteLine($"Cantidad Exixtencia:_ {cantidadExistencia}");
+      
+    }
 
 
 }
